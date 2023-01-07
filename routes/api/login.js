c01024b3163fd92be010a8bd93ca29852931886a
kongtaehyeon-khu <kong3047@khu.ac.kr>
2023-01-07 23:34:34 +0900

    Swagger for Login API

--- routes/api/login.js
+++ routes/api/login.js
@@ -3,6 +3,44 @@ const User = require("../../models/User.js");
 
 const router = express.Router();
 
+/**
+ * @swagger
+ * paths:
+ *  /api/login:
+ *   post:
+ *    tags:
+ *    - user
+ *    description: 유저 로그인
+ *    parameters:
+ *    - in: body
+ *      name: body
+ *      required: true
+ *
+ *      schema:
+ *       properties:
+ *        id:
+ *         type: string
+ *        password:
+ *         type: string
+ *
+ *    responses:
+ *     200:
+ *      description: 로그인 성공.
+ *      schema:
+ *       properties:
+ *        Message:
+ *         type: string
+ *     401:
+ *      description: 아이디 또는 비밀번호가 일치하지 않는다.
+ *      schema:
+ *       properties:
+ *        Message:
+ *         type: string
+ *     500:
+ *      description: 서버 에러.
+ *
+ *
+ */
 router.post("/", async (req, res) => {
   const { id, password } = req.body;
   try {
@@ -11,7 +49,7 @@ router.post("/", async (req, res) => {
       res.send("Login Success");
       return;
     }
-    res.send({
+    res.status(401).send({
       errors: [{ Message: "Can't Find User, Please Check ID and Password" }],
     });
   } catch (error) {
